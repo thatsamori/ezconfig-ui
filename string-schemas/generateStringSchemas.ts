b7faaa3ebb07b0a65230986278f7e6f3e516885f
Chris Amori <chris.amori93@gmail.com>
2026-01-27 13:43:18 -0800

    docs(25): create phase plan for UI simplification
    Phase 25: UI Simplification
    - 2 plans created
    - 6 tasks defined
    - Ready for execution

---
+++ string-schemas/generateStringSchemas.ts
@@ -0,0 +1,21 @@
+import { WEAPON_CONFIG_OPTIONS } from "@/lib/config/weaponConfigSchema";
+import { CHARACTER_CONFIG_OPTIONS } from "@/lib/config/characterConfigSchema";
+import { writeFileSync } from "node:fs";
+import { resolve } from "node:path/win32";
+import { DataType } from "@/lib/config/types";
+
+const writeWeaponConfigStringSchema = () => {
+  let acc = "";
+
+  const options = Object.values(WEAPON_CONFIG_OPTIONS).flat();
+  options.forEach((opt) => {
+    const isVector =
+      opt.dataType === DataType.Vector || opt.dataType === DataType.Vector2D;
+    acc += `${opt.configKey},${isVector ? "Vector" : opt.dataType} `;
+  });
+
+  // Write to file
+  Bun.write("./weaponConfigSchema.txt", acc.trim());
+};
+
+writeWeaponConfigStringSchema();
